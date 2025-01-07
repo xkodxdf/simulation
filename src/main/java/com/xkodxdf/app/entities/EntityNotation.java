d6d4370c8a2e196ac6629e3be36345989deb19f8
xkodxdf <sirota92@bk.ru>
2025-01-07 19:01:27 +0300

    add[enum for entities notation]

---
+++ src/main/java/com/xkodxdf/app/entities/EntityNotation.java
@@ -0,0 +1,24 @@
+package com.xkodxdf.app.entities;
+
+public enum EntityNotation {
+
+    EMPTY_SQUARE(" . . "),
+    GRASS(" .G. "),
+    ROCK(" .R. "),
+    TREE(" .T. "),
+    HERBIVORE(" .H. "),
+    PREDATOR(" .P. ");
+
+
+    private final String notation;
+
+
+    EntityNotation(String notation) {
+        this.notation = notation;
+    }
+
+
+    public String getNotation() {
+        return notation;
+    }
+}
