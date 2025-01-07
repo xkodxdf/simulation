2032f693b139430ea4cf1296b62b8742160b703b
xkodxdf <sirota92@bk.ru>
2025-01-07 19:04:59 +0300

    add[coordinates for the location of entities on the map]

---
+++ src/main/java/com/xkodxdf/app/map/Coordinates.java
@@ -0,0 +1,37 @@
+package com.xkodxdf.app.map;
+
+import java.util.Objects;
+
+public class Coordinates {
+
+    private final int x;
+    private final int y;
+
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return getX() == that.getX() && getY() == that.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+}
