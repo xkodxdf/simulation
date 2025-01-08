ddc90d783485c8129a61fd1551b07f37cd894708
xkodxdf <sirota92@bk.ru>
2025-01-08 17:55:24 +0300

    add methods for placing, receiving and removing creatures from the map, getting the size of the map, and verifying the validity of coordinates

--- src/main/java/com/xkodxdf/app/map/WorldMap.java
+++ src/main/java/com/xkodxdf/app/map/WorldMap.java
@@ -1,14 +1,13 @@
 package com.xkodxdf.app.map;
 
+import com.xkodxdf.app.Messages;
 import com.xkodxdf.app.entities.base.Entity;
-import com.xkodxdf.app.entities.inanimate.EmptySquare;
+import com.xkodxdf.app.exceptions.InvalidCoordinatesException;
+import com.xkodxdf.app.exceptions.InvalidMapParametersException;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-    Карта, содержит в себе коллекцию для хранения существ и их расположения.
-*/
 public class WorldMap {
 
     private final int width;
@@ -16,7 +15,19 @@ public class WorldMap {
     private final Map<Coordinates, Entity> map = new HashMap<>();
 
 
-    public WorldMap(int width, int height) {
+    public WorldMap() {
+        this.width = 32;
+        this.height = 16;
+    }
+
+    public WorldMap(int width, int height) throws InvalidMapParametersException {
+        int minWidthHeightValue = 8;
+        int maxWidthHeightValue = 48;
+        if ((width < minWidthHeightValue) || (width > maxWidthHeightValue)
+                || (height < minWidthHeightValue) || (height > maxWidthHeightValue)) {
+            throw new InvalidMapParametersException(String.format(Messages.invalidMapParametersMsg + "%d - %d",
+                    minWidthHeightValue, maxWidthHeightValue));
+        }
         this.width = width;
         this.height = height;
     }
@@ -30,16 +41,45 @@ public class WorldMap {
         return height;
     }
 
-    public Map<Coordinates, Entity> getMap() {
-        return map;
-    }
-
 
     public void initMap() {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                map.put(new Coordinates(x, y), new EmptySquare());
+                map.put(new Coordinates(x, y), null);
             }
+        }
+    }
+
+    public int getSize() {
+        return map.size();
+    }
+
+    public void setEntity(Coordinates coordinates, Entity entity) throws InvalidCoordinatesException {
+        validateCoordinates(coordinates);
+        map.put(coordinates, entity);
+    }
+
+    public Entity getEntity(Coordinates coordinates) throws InvalidCoordinatesException {
+        validateCoordinates(coordinates);
+        return map.get(coordinates);
+    }
+
+    public void removeEntity(Coordinates coordinates) throws InvalidCoordinatesException {
+        validateCoordinates(coordinates);
+        map.remove(coordinates);
+    }
+
+    private boolean isCoordinatesValid(Coordinates coordinates) {
+        int x = coordinates.getX();
+        int y = coordinates.getY();
+
+        return ((x >= 0) && (y >= 0) && (x < width) && (y < height));
+    }
+
+    private void validateCoordinates(Coordinates coordinates) throws InvalidCoordinatesException {
+        if (!isCoordinatesValid(coordinates)) {
+            throw new InvalidCoordinatesException(Messages.invalidCoordinatesMsg
+                    + coordinates.getX() + ":" + coordinates.getY());
         }
     }
 }
