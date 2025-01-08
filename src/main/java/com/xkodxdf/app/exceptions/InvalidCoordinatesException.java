0ff4682a6b300bfe05c9e14b3e8ff0141ddef320
xkodxdf <sirota92@bk.ru>
2025-01-08 17:46:24 +0300

    add[exceptions for invalid coordinates/map parameters]

---
+++ src/main/java/com/xkodxdf/app/exceptions/InvalidCoordinatesException.java
@@ -0,0 +1,8 @@
+package com.xkodxdf.app.exceptions;
+
+public class InvalidCoordinatesException extends Exception {
+
+    public InvalidCoordinatesException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/xkodxdf/app/exceptions/InvalidMapParametersException.java
@@ -0,0 +1,8 @@
+package com.xkodxdf.app.exceptions;
+
+public class InvalidMapParametersException extends Exception {
+
+    public InvalidMapParametersException(String message) {
+        super(message);
+    }
+}
