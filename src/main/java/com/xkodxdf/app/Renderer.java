13e4974c57beaa47ecb67756ec327f75b281dc75
xkodxdf <sirota92@bk.ru>
2025-01-09 15:23:18 +0300

    change[split renderMap() into several methods and determine the identity of an entity using instanceof]

--- src/main/java/com/xkodxdf/app/Renderer.java
+++ src/main/java/com/xkodxdf/app/Renderer.java
@@ -2,10 +2,14 @@ package com.xkodxdf.app;
 
 import com.xkodxdf.app.entities.EntityNotation;
 import com.xkodxdf.app.entities.base.Entity;
+import com.xkodxdf.app.entities.animated.*;
+import com.xkodxdf.app.entities.inanimate.*;
+import com.xkodxdf.app.exceptions.InvalidCoordinatesException;
 import com.xkodxdf.app.map.Coordinates;
 import com.xkodxdf.app.map.WorldMap;
 
-import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Renderer {
 
@@ -17,37 +21,47 @@ public class Renderer {
     }
 
 
-    public void renderMap() {
-        Map<com.xkodxdf.app.map.Coordinates, Entity> map = worldMap.getMap();
-        StringBuilder line = new StringBuilder();
+    public void renderMap() throws InvalidCoordinatesException {
+        StringBuilder row = new StringBuilder();
+        System.out.print("\n   +");
+        IntStream.range(0, worldMap.getWidth()).forEach(num -> System.out.printf(" %2d ", num));
+        System.out.println();
         for (int y = 0; y < worldMap.getHeight(); y++) {
-            for (int x = 0; x < worldMap.getWidth(); x++) {
-                Entity entity = map.get(new Coordinates(x, y));
-                switch (entity.getClass().getSimpleName()) {
-                    case "EmptySquare":
-                        line.append(EntityNotation.EMPTY_SQUARE.getNotation());
-                        break;
-                    case "Grass":
-                        line.append(EntityNotation.GRASS.getNotation());
-                        break;
-                    case "Rock":
-                        line.append(EntityNotation.ROCK.getNotation());
-                        break;
-                    case "Tree":
-                        line.append(EntityNotation.TREE.getNotation());
-                        break;
-                    case "Herbivore":
-                        line.append(EntityNotation.HERBIVORE.getNotation());
-                        break;
-                    case "Predator":
-                        line.append(EntityNotation.PREDATOR.getNotation());
-                        break;
-                    default:
-                        throw new IllegalStateException();
-                }
-            }
-            System.out.println(line);
-            line.setLength(0);
+            System.out.printf("%2d ", y);
+            System.out.println(assembleRow(row, y));
+            row.setLength(0);
         }
+    }
+
+    private String assembleRow(StringBuilder row, int y) throws InvalidCoordinatesException {
+        for (int x = 0; x < worldMap.getWidth(); x++) {
+            Entity entity = worldMap.getEntity(new Coordinates(x, y));
+            row.append(" . ").append(getEntityNotation(entity));
+        }
+        row.append(" .");
+
+        return row.toString();
+    }
+
+    private String getEntityNotation(Entity entity) {
+        if (Objects.isNull(entity)) {
+            return EntityNotation.EMPTY_SQUARE.getNotation();
+        }
+        if (entity instanceof Grass) {
+            return EntityNotation.GRASS.getNotation();
+        }
+        if (entity instanceof Rock) {
+            return EntityNotation.ROCK.getNotation();
+        }
+        if (entity instanceof Tree) {
+            return EntityNotation.TREE.getNotation();
+        }
+        if (entity instanceof Herbivore) {
+            return EntityNotation.HERBIVORE.getNotation();
+        }
+        if (entity instanceof Predator) {
+            return EntityNotation.PREDATOR.getNotation();
+        }
+        throw new IllegalStateException(Messages.noNotationForEntity + entity.getClass().getSimpleName());
     }
 }
