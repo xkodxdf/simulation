9f9542af9360277b475742a8595a653366245a90
xkodxdf <sirota92@bk.ru>
2025-01-09 15:24:26 +0300

    add[not found entity notation message]

--- src/main/java/com/xkodxdf/app/Messages.java
+++ src/main/java/com/xkodxdf/app/Messages.java
@@ -3,6 +3,7 @@ package com.xkodxdf.app;
 public class Messages {
 
     public static final String invalidCoordinatesMsg = "Incorrect coordinates are specified: ";
+    public static final String noNotationForEntity = "No notation found for the entity: ";
     public static final String invalidMapParametersMsg = "the value of the width and height parameters " +
             "should be in the range of ";
 }
