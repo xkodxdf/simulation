355dbb0d3621c52d82735d8a3dd12531adb2ac36
xkodxdf <sirota92@bk.ru>
2025-01-09 17:59:42 +0300

    rename[InvalidMapParametersException -> InvalidParametersException]

---
+++ src/main/java/com/xkodxdf/app/exceptions/InvalidParametersException.java
@@ -0,0 +1,8 @@
+package com.xkodxdf.app.exceptions;
+
+public class InvalidParametersException extends Exception {
+
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+}
