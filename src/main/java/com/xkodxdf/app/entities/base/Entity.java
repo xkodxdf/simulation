cdef856473d10e8ba8acf8e59f2a0a58b842453c
xkodxdf <sirota92@bk.ru>
2025-01-15 17:49:31 +0300

    add[id for entities]

--- src/main/java/com/xkodxdf/app/entities/base/Entity.java
+++ src/main/java/com/xkodxdf/app/entities/base/Entity.java
@@ -1,6 +1,22 @@
 package com.xkodxdf.app.entities.base;
+
 /*
     Корневой абстрактный класс для всех существ и объектов существующих в симуляции.
 */
 public abstract class Entity {
+
+    private static int nextId = 1;
+
+    protected final int id;
+
+
+    public Entity() {
+        id = nextId;
+        nextId++;
+    }
+
+
+    public int getId() {
+        return id;
+    }
 }
