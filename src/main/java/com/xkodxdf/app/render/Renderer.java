8e4b399b2c4e35f3f3a9a38710c45289a828031d
xkodxdf <sirota92@bk.ru>
2025-01-21 15:11:02 +0300

    change[use world map itself, not the world map manager]

--- src/main/java/com/xkodxdf/app/render/Renderer.java
+++ src/main/java/com/xkodxdf/app/render/Renderer.java
@@ -8,34 +8,26 @@ import com.xkodxdf.app.entities.inanimate.Corpse;
 import com.xkodxdf.app.entities.inanimate.Grass;
 import com.xkodxdf.app.entities.inanimate.Rock;
 import com.xkodxdf.app.entities.inanimate.Tree;
-import com.xkodxdf.app.exceptions.InvalidParametersException;
 import com.xkodxdf.app.map.Coordinates;
-import com.xkodxdf.app.map.Manager;
+import com.xkodxdf.app.map.config.Config;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class Renderer {
 
-    private final Manager mapManager;
-
-
-    public Renderer(Manager mapManager) {
-        this.mapManager = mapManager;
-    }
-
-
-    public void renderMap() throws InvalidParametersException {
+    public void renderMap(Map<Coordinates, Entity> map) {
         StringBuilder row = new StringBuilder();
         System.out.println();
-        for (int y = 0; y < mapManager.getHeight(); y++) {
-            System.out.println(assembleRow(row, y));
+        for (int y = 0; y < Config.getConfig().getHeight(); y++) {
+            System.out.println(assembleRow(row, y, map));
             row.setLength(0);
         }
     }
 
-    private String assembleRow(StringBuilder row, int y) throws InvalidParametersException {
-        for (int x = 0; x < mapManager.getWidth(); x++) {
-            Entity entity = mapManager.getEntity(new Coordinates(x, y));
+    private String assembleRow(StringBuilder row, int y, Map<Coordinates, Entity> map) {
+        for (int x = 0; x < Config.getConfig().getWidth(); x++) {
+            Entity entity = map.get(new Coordinates(x, y));
             row.append(getEntityNotation(entity)).append(" ");
         }
         return row.toString();
