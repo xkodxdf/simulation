cb68273e23e6ebb7d0931a61d0660537dcbdd6e2
xkodxdf <sirota92@bk.ru>
2025-01-21 19:48:40 +0300

    add[singleton for roadblocks]

--- src/main/java/com/xkodxdf/app/entities/inanimate/Rock.java
+++ src/main/java/com/xkodxdf/app/entities/inanimate/Rock.java
@@ -1,4 +1,21 @@
 package com.xkodxdf.app.entities.inanimate;
 
-public class Rock {
+import com.xkodxdf.app.entities.base.Entity;
+
+public class Rock extends Entity {
+
+    private static Rock instance;
+
+
+    private Rock() {
+    }
+
+
+    public static Rock getInstance() {
+        if (instance == null) {
+            instance = new Rock();
+        }
+
+        return instance;
+    }
 }
--- src/main/java/com/xkodxdf/app/entities/inanimate/Tree.java
+++ src/main/java/com/xkodxdf/app/entities/inanimate/Tree.java
@@ -1,4 +1,21 @@
 package com.xkodxdf.app.entities.inanimate;
 
-public class Tree {
+import com.xkodxdf.app.entities.base.Entity;
+
+public class Tree extends Entity {
+
+    private static Tree instance;
+
+
+    private Tree() {
+    }
+
+
+    public static Tree getInstance() {
+        if (instance == null) {
+            instance = new Tree();
+        }
+
+        return instance;
+    }
 }
