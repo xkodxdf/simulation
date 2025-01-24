7fa60b42fcda8cb3424efe578713450c3ca1121f
xkodxdf <sirota92@bk.ru>
2025-01-24 19:19:20 +0300

    add[entity for corpses]

---
+++ src/main/java/com/xkodxdf/app/entities/inanimate/Corpse.java
@@ -0,0 +1,20 @@
+package com.xkodxdf.app.entities.inanimate;
+
+import com.xkodxdf.app.entities.base.Entity;
+
+public class Corpse extends Entity {
+
+    private int decayCounter = 4;
+
+
+    public int getDecayCounter() {
+        return decayCounter;
+    }
+
+
+    public void decay() {
+        if (decayCounter > 0) {
+            decayCounter--;
+        }
+    }
+}
