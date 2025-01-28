903dd4fbf32b609126305eb24f847f9cbb6e3094
xkodxdf <sirota92@bk.ru>
2025-01-28 15:49:12 +0300

    add[equals, hashCode]

--- src/main/java/com/xkodxdf/app/entities/base/Entity.java
+++ src/main/java/com/xkodxdf/app/entities/base/Entity.java
@@ -1,22 +1,31 @@
 package com.xkodxdf.app.entities.base;
 
-/*
-    Корневой абстрактный класс для всех существ и объектов существующих в симуляции.
-*/
+import java.util.Objects;
+
 public abstract class Entity {
 
     private static int nextId = 1;
 
     protected final int id;
 
-
     public Entity() {
         id = nextId;
         nextId++;
     }
 
-
     public int getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
