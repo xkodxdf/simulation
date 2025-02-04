0bacc47c1e7e57501107756cb9704a3c80465aef
xkodxdf <sirota92@bk.ru>
2025-02-04 14:33:34 +0300

    ref[constant names to uppercase]

--- src/main/java/com/xkodxdf/app/messages/Messages.java
+++ src/main/java/com/xkodxdf/app/messages/Messages.java
@@ -2,13 +2,11 @@ package com.xkodxdf.app.messages;
 
 public class Messages {
 
-    public static final String mustNotBeNull = " must not be null";
-    public static final String invalidCoordinatesMsg = "Incorrect coordinates are specified: ";
-    public static final String noNotationForEntity = "No notation found for the entity: ";
-    public static final String invalidEntityType = "Invalid entity type: ";
-    public static final String invalidCreatureState = "Invalid creature state: ";
-    public static final String inanimateFillingPercentageErr = "The percentage of filling cannot be negative or greater " +
-            "than 60 for all inanimate objects.";
-    public static final String invalidMapParametersMsg = "The value of the width and height parameters " +
-            "should be in the range of ";
+    public static final String MUST_NOT_BE_NULL = " must not be null";
+    public static final String INCORRECT_COORDINATES_ARE_SPECIFIED = "Incorrect coordinates are specified: ";
+    public static final String NO_NOTATION_FOUND_FOR_ENTITY = "No notation found for the entity: ";
+    public static final String INVALID_ENTITY_TYPE = "Invalid entity type: ";
+    public static final String INVALID_CREATURE_STATE = "Invalid creature state: ";
+    public static final String ENTITIES_FILLING_PERCENTAGE_ERR = "Invalid map filling percentage parameter ";
+    public static final String INVALID_MAP_SIZE_PARAMETERS = "Invalid map width or height parameter ";
 }
