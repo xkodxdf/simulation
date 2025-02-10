3115591bc060a107efe19f4ce3f8ada8a313d36c
xkodxdf <sirota92@bk.ru>
2025-02-10 17:04:57 +0300

    change[remake sim launch according to added menu]

--- src/main/java/com/xkodxdf/app/Main.java
+++ src/main/java/com/xkodxdf/app/Main.java
@@ -1,11 +1,19 @@
 package com.xkodxdf.app;
 
-import com.xkodxdf.app.exceptions.InvalidParametersException;
+import com.xkodxdf.app.menu.Exit;
+import com.xkodxdf.app.menu.MainMenu;
+import com.xkodxdf.app.menu.Menu;
 
 public class Main {
 
-    public static void main(String[] args) throws InvalidParametersException, InterruptedException {
-        Simulation sim = new Simulation();
-        sim.start();
+    public static void main(String[] args) throws Exception {
+        SimulationManage simulationManage = new SimulationManage();
+        Menu currentMenu = new MainMenu(simulationManage);
+        while (!currentMenu.getClass().equals(Exit.class)) {
+            currentMenu.display();
+            currentMenu = currentMenu.selectItem();
+        }
+        currentMenu.display();
+        ((Exit) currentMenu).closeScanner();
     }
 }
