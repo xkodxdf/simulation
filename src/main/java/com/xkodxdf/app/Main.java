1a11f842fe70d8dafedffce4d4f327c9d9917661
xkodxdf <sirota92@bk.ru>
2025-02-12 18:04:47 +0300

    change[create reader in Main]

--- src/main/java/com/xkodxdf/app/Main.java
+++ src/main/java/com/xkodxdf/app/Main.java
@@ -1,19 +1,25 @@
 package com.xkodxdf.app;
 
+import com.xkodxdf.app.input.IntegerInput;
+import com.xkodxdf.app.input.StringInput;
 import com.xkodxdf.app.menu.Exit;
 import com.xkodxdf.app.menu.MainMenu;
 import com.xkodxdf.app.menu.Menu;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        SimulationManage simulationManage = new SimulationManage();
-        Menu currentMenu = new MainMenu(simulationManage);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        SimulationManage simulationManage = new SimulationManage(new StringInput(reader));
+        Menu currentMenu = new MainMenu(new IntegerInput(reader), simulationManage);
         while (!currentMenu.getClass().equals(Exit.class)) {
             currentMenu.display();
             currentMenu = currentMenu.selectItem();
         }
         currentMenu.display();
-        ((Exit) currentMenu).closeScanner();
+        reader.close();
     }
 }
