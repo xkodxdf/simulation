efc6d4719cca511846c8f7ef38db9595bfc1d81a
xkodxdf <sirota92@bk.ru>
2025-02-12 16:40:08 +0300

    add[text constants for simulation pause]

---
+++ src/main/java/com/xkodxdf/app/text_constants/SimulationPauseMessages.java
@@ -0,0 +1,9 @@
+package com.xkodxdf.app.text_constants;
+
+public class SimulationPauseMessages {
+
+    public static final String PROMPT_MSG = "\n-Press ENTER to PAUSE/RESUME\n-Input anything text to return main menu";
+    public static final String PAUSED = "Simulation paused";
+    public static final String RESUMED = "Simulation resumed";
+    public static final String STOPPED = "Simulation stopped";
+}
